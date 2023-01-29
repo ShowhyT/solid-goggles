0faeae885b5ac3b37f706da0963503e02a2b4a40
ShowhyT <109768004+ShowhyT@users.noreply.github.com>
2023-01-29 13:40:53 +0300

    Remane

--- discord/main.py
+++ discord/main.py
@@ -23,7 +23,7 @@ async def on_ready():
     print(f"{len(bot.guilds)} servers, {len(bot.users)} users \nБот запущен!" )
 
 async def load():
-    path_to_file = os.path.join(os.getenv("GITHUB_WORKSPACE"), "discord", "cogs")
+    path_to_file = os.path.join(os.getenv("TOKEN"))
     for filename in os.listdir(path_to_file):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
